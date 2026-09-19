91c91898d124b6cc654ed35288e953fa5f994b46
ghazalehalirezaloo <ghazalehalirezaloo@gmail.com>
2026-09-19 19:54:34 +0330

    Added Context + installed NuGets

--- MyEshop/Program.cs
+++ MyEshop/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MyEshop.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+#region Db Context 
+builder.Services.AddDbContext<MyEshopContext>(options =>
+ options.UseSqlServer("Data Source = GHAZALEH\\SQLEXPRESS; Initial Catalog = EshopCore_DB ; Integrated Security = True; TrustServerCertificate=True;"));
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
