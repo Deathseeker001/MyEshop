e4eecda259250f336ed49e20e11da6deb2658c95
ghazalehalirezaloo <ghazalehalirezaloo@gmail.com>
2026-09-19 21:22:13 +0330

    Seed Data to Category

--- MyEshop/Data/MyEshopContext.cs
+++ MyEshop/Data/MyEshopContext.cs
@@ -10,5 +10,43 @@ namespace MyEshop.Data
             
         }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region Seed Data Category
+
+            modelBuilder.Entity<Category>().HasData(new Category()
+            {
+                Id = 1,
+                Name = "Asp.Net Core",
+                Description = "Asp.Net Core 9"
+
+            },
+            new Category()
+            {
+                Id = 2,
+                Name = "لباس ورزشی ",
+                Description = "گروه لباس ورزشی"
+
+            },
+            new Category()
+            {
+                Id = 3,
+                Name = "ساعت مچی",
+                Description = "ساعت مچی"
+
+            },
+            new Category()
+            {
+                Id = 4,
+                Name = "لوازم منزل",
+                Description = "لوازم منزل"
+
+            }
+                );
+
+            #endregion
+           base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ MyEshop/Migrations/20260919174955_SeedDataCategoryMig.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MyEshop.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataCategoryMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Asp.Net Core 9", "Asp.Net Core" },
+                    { 2, "گروه لباس ورزشی", "لباس ورزشی " },
+                    { 3, "ساعت مچی", "ساعت مچی" },
+                    { 4, "لوازم منزل", "لوازم منزل" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
