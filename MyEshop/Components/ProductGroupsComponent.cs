d4ea28c1b843f1e17c1fc95dd2050c7c1fd04a7e
ghazalehalirezaloo <ghazalehalirezaloo@gmail.com>
2026-09-23 14:59:27 +0330

    View Component

---
+++ MyEshop/Components/ProductGroupsComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyEshop.Data;
+
+namespace MyEshop.Components
+{
+    public class ProductGroupsComponent : ViewComponent
+    {
+        private MyEshopContext _context;
+        public ProductGroupsComponent(MyEshopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("/Views/Components/ProductGroupsComponent.cshtml", _context.Categories);
+        }
+    }
+}
